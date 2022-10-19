0cdd5018f5661707f6d5bf91a3f4253ef4de5d5d
xxh_wit <861411454@qq.com>
2022-10-19 21:52:52 +0800

    small change

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,3 @@
-from .utils import separate_population_data, align_time_dimension, \
-    separate_population_with_interval, divide_data_with_bins, \
-    calculate_pearson_matrix, split_data_via_stim
+from .utils import separate_population_data, align_time_dimension, separate_population_with_interval, divide_data_with_bins, calculate_pearson_matrix, split_data_via_stim
 from .draw import draw_pearson_matrix
 
